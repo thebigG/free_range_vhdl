b2796f554badf7acb8d3edbd1015c9369175ad97
thebigg <yunior.eury@gmail.com>
2021-08-08 22:29:34 -0500

    Chapter 4.

--- ch4/listing_4_1.vhdl
+++ ch4/listing_4_1.vhdl
@@ -13,6 +13,11 @@ end my_circuit;
 architecture my_circuit_arc of my_circuit is
     signal A_out, B_out, C_out : std_logic;
 
+-- In VHDL, statements are not executed stet-by-step asn in Java/C/C++.
+-- Everything gets executed concurrently at the same time.
+-- Meaning one could change the order of these statements, and it will
+-- still have the same effect on the circuitry/FPGA.
+-- Which makes a lot of sense in the context of gates.
 begin
     A_out <= A_1 and A_2;
     B_out <= B_1 or B_2;
